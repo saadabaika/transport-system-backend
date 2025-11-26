22f8052d8d3e826e88bf4187a814cc50a5536908
Saad Abaika <saadabaikaa@gmail.com>
2025-11-26 13:07:09 +0100

    Fix: Use SQLite temporarily to resolve psycopg2 issue

--- config/settings.py
+++ config/settings.py
@@ -85,12 +85,12 @@ TEMPLATES = [
  #  }
 
 
-#DATABASES = {
- #    'default': {
-  #         'ENGINE': 'django.db.backends.sqlite3',
-   #       'NAME': BASE_DIR / 'db.sqlite3',
-   #    }
- # }
+DATABASES = {
+    'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': BASE_DIR / 'db.sqlite3',
+       }
+  }
 import os
 
 # Configuration PostgreSQL Render
